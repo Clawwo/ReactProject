ba75aee4d349c7e0dbf8d4e19d9c7a4d4f976b3f
Clawwo <genzz760@gmail.com>
2025-02-10 18:40:45 +0700

    Conditional Rendering

--- latihan-react/src/Pages/login.jsx
+++ latihan-react/src/Pages/login.jsx
@@ -1,20 +1,10 @@
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 import FormLogin from "../components/Fragments/FormLogin";
-import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <AuthLayouts title="Login">
+    <AuthLayouts title="Login" type="login">
       <FormLogin />
-      <p className="mt-4 text-sm text-center text-gray-500">
-        Belum punya akun?{" "}
-        <Link
-          to="/register"
-          className="font-semibold text-blue-600 hover:underline"
-        >
-          Daftar di sini
-        </Link>
-      </p>
     </AuthLayouts>
   );
 };
