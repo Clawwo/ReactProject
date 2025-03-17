883e4b50936838fd352bfcf1aba877bd5504fc0c
Farel Haryanto <genzz760@gmail.com>
2025-03-17 12:15:54 +0700

    Delete latihan-react directory

--- latihan-react/src/components/Elements/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function MyButton(props) {
-  const { warna = "bg-blue-500", children = "Button" } = props;
-  return (
-    <button className={`${warna} text-white px-4 py-2 rounded`}>
-      {children}
-    </button>
-  );
-}
-
